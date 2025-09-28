7f941e87c3461de90fb556785d7b7cccd3169b91
CrazyBoy <xspeed1989@gmail.com>
2025-09-28 22:20:03 +0800

    feat: Add command handling for directory creation and file upload in session management

--- src/data_define.rs
+++ src/data_define.rs
@@ -1,7 +1,14 @@
+use std::vec::Vec;
+use prost::Message;
+use crc32c::crc32c;
+
 pub(crate) const PACKET_HEADER_SIZE: usize = 12; // 4 bytes command, 4 bytes length, 4 bytes checksum
 pub(crate) const MAX_PAYLOAD_SIZE: usize = 1 * 1024 * 1024; // 10 MB
 pub(crate) enum Command {
     CmdAuthenticate = 1,
+    CmdMkDir,
+    CmdStartUpload,
+    CmdUploadChunk,
 }
 
 impl TryInto<Command> for u32 {
@@ -13,4 +20,30 @@ impl TryInto<Command> for u32 {
             _ => Err("Unknown command"),
         }
     }
+}
+
+pub(crate) fn package_data(cmd: Command, msg: impl Message) -> Vec<u8> 
+{
+    let msg_len = msg.encoded_len();
+    let data_len = PACKET_HEADER_SIZE + msg_len;
+    let mut data_package = Vec::with_capacity(data_len);
+
+    data_package.extend_from_slice(&(cmd as u32).to_le_bytes());
+    data_package.extend_from_slice(&(msg_len as u32).to_le_bytes());
+
+    // Reserve space for checksum (4 bytes)
+    data_package.extend_from_slice(&[0u8; 4]);
+
+    // Encode protobuf directly into the buffer to avoid extra copy
+    msg.encode(&mut data_package).unwrap();
+
+    // Calculate checksum for the protobuf part
+    let checksum = crc32c(&data_package[PACKET_HEADER_SIZE..]);
+
+    // Write checksum into the reserved space
+    let checksum_bytes = checksum.to_le_bytes();
+    let checksum_pos = 8; 
+    data_package[checksum_pos..checksum_pos + 4].copy_from_slice(&checksum_bytes);
+
+    data_package
 }
--- src/server/session.rs
+++ src/server/session.rs
@@ -1,32 +1,51 @@
+use crate::data_define::*;
 use prost::Message;
 use std::collections::VecDeque;
-use std::sync::{Arc, Condvar, Mutex as StdMutex};
+use std::sync::{Arc, Condvar};
 use tokio::io::{AsyncReadExt, AsyncWriteExt, ReadHalf, WriteHalf, split};
 use tokio::net::TcpStream;
-use tokio::sync::oneshot;
+use tokio::sync::{Mutex as AsyncMutex, Notify, oneshot};
 use tokio::task::JoinSet;
 use tokio_rustls::{TlsAcceptor, server::TlsStream};
-
 mod file_deploy {
     include!(concat!(env!("OUT_DIR"), "/file_deploy.rs"));
 }
 
 use crate::data_define;
 use crate::server::config::get_config;
+
 const MAX_QUEUE_LEN: usize = 128;
+
 pub(crate) struct Session {
-    authenticated: StdMutex<bool>,
-    write_queue: StdMutex<VecDeque<Vec<u8>>>,
-    cond_var: Condvar,
+    authenticated: AsyncMutex<bool>,
+    write_queue: AsyncMutex<VecDeque<Vec<u8>>>,
+    notify: Notify,
+    file_handles: AsyncMutex<std::collections::HashMap<String, tokio::fs::File>>,
+}
+
+fn is_valid_path(path: &str) -> bool {
+    let config = get_config();
+    for white_dir in config.whitelisted_dirs {
+        let white_dir = white_dir.to_str().unwrap().to_string();
+        if path.starts_with(white_dir.trim_end_matches(std::path::MAIN_SEPARATOR)) {
+            return true;
+        }
+    }
+    false
 }
 
 impl Session {
     pub(crate) fn new() -> Self {
         Session {
-            authenticated: StdMutex::new(false),
-            write_queue: StdMutex::new(VecDeque::new()),
-            cond_var: Condvar::new(),
+            authenticated: AsyncMutex::new(false),
+            write_queue: AsyncMutex::new(VecDeque::new()),
+            notify: Notify::new(),
+            file_handles: AsyncMutex::new(std::collections::HashMap::new()),
         }
+    }
+
+    async fn is_authenticated(self: Arc<Self>) -> bool {
+        *self.authenticated.lock().await
     }
 
     async fn on_auth(self: Arc<Self>, payload: &[u8]) {
@@ -35,22 +54,184 @@ impl Session {
             let auth_req = auth_req.unwrap();
             let config = get_config();
             if auth_req.password == config.password {
-                *self.authenticated.lock().unwrap() = true;
+                *self.authenticated.lock().await = true;
             }
         }
         // 发送认证结果
         let auth_resp = file_deploy::AuthResponse {
-            success: *self.authenticated.lock().unwrap(),
+            success: *self.authenticated.lock().await,
         };
-        let mut resp_buf = Vec::new();
-        auth_resp.encode(&mut resp_buf).unwrap();
-        let mut packet = Vec::with_capacity(data_define::PACKET_HEADER_SIZE + resp_buf.len());
+        let packet = package_data(Command::CmdAuthenticate, auth_resp);
+        self.post_send(packet).await;
+    }
 
-        packet.extend_from_slice(&(data_define::Command::CmdAuthenticate as u32).to_le_bytes());
-        packet.extend_from_slice(&(resp_buf.len() as u32).to_le_bytes());
-        let checksum = crc32c::crc32c(&resp_buf);
-        packet.extend_from_slice(&checksum.to_le_bytes());
-        packet.extend_from_slice(&resp_buf);
+    async fn on_mkdir(self: Arc<Self>, payload: &[u8]) {
+        if !self.clone().is_authenticated().await {
+            let resp = file_deploy::MkDirResponse {
+                success: false,
+                error: "Not authenticated".to_string(),
+            };
+            let packet = package_data(Command::CmdMkDir, resp);
+            self.post_send(packet).await;
+            return;
+        }
+        let mk_dir_req = file_deploy::MkDirRequest::decode(payload);
+        if !mk_dir_req.is_ok() {
+            let resp = file_deploy::MkDirResponse {
+                success: false,
+                error: "Invalid request".to_string(),
+            };
+            let packet = package_data(Command::CmdMkDir, resp);
+            self.post_send(packet).await;
+            return;
+        }
+        let mk_dir_req = mk_dir_req.unwrap();
+        if !is_valid_path(&mk_dir_req.absolute_path) {
+            let resp = file_deploy::MkDirResponse {
+                success: false,
+                error: "Path not allowed".to_string(),
+            };
+            let packet = package_data(Command::CmdMkDir, resp);
+            self.post_send(packet).await;
+            return;
+        }
+        let result = tokio::fs::create_dir_all(&mk_dir_req.absolute_path).await;
+        let resp = if result.is_ok() {
+            file_deploy::MkDirResponse {
+                success: true,
+                error: "".to_string(),
+            }
+        } else {
+            file_deploy::MkDirResponse {
+                success: false,
+                error: format!("Failed to create directory: {}", result.err().unwrap()),
+            }
+        };
+        let packet = package_data(Command::CmdMkDir, resp);
+        self.post_send(packet).await;
+    }
+
+    async fn on_start_upload(self: Arc<Self>, payload: &[u8]) {
+        if !self.clone().is_authenticated().await {
+            let resp = file_deploy::StartUploadResponse {
+                success: false,
+                error: "Not authenticated".to_string(),
+            };
+            let packet = package_data(Command::CmdStartUpload, resp);
+            self.post_send(packet).await;
+            return;
+        }
+
+        let start_upload_req = file_deploy::StartUploadRequest::decode(payload);
+        if !start_upload_req.is_ok() {
+            let resp = file_deploy::StartUploadResponse {
+                success: false,
+                error: "Invalid request".to_string(),
+            };
+            let packet = package_data(Command::CmdStartUpload, resp);
+            self.post_send(packet).await;
+            return;
+        }
+        let start_upload_req = start_upload_req.unwrap();
+
+        if !is_valid_path(&start_upload_req.absolute_path) {
+            let resp: file_deploy::StartUploadResponse = file_deploy::StartUploadResponse {
+                success: false,
+                error: "Path not allowed".to_string(),
+            };
+            let packet = package_data(Command::CmdStartUpload, resp);
+            self.post_send(packet).await;
+            return;
+        }
+        let file = tokio::fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&start_upload_req.absolute_path)
+            .await;
+        if file.is_err() {
+            let resp = file_deploy::StartUploadResponse {
+                success: false,
+                error: format!("Failed to open file: {}", file.err().unwrap()),
+            };
+            let packet = package_data(Command::CmdStartUpload, resp);
+            self.post_send(packet).await;
+            return;
+        }
+        {
+            self.file_handles
+                .lock()
+                .await
+                .insert(start_upload_req.absolute_path.clone(), file.unwrap());
+        }
+        let resp = file_deploy::StartUploadResponse {
+            success: true,
+            error: "".to_string(),
+        };
+        let packet = package_data(Command::CmdStartUpload, resp);
+        self.post_send(packet).await;
+    }
+
+    async fn on_upload_chunk(self: Arc<Self>, payload: &[u8]) {
+        if !self.clone().is_authenticated().await {
+            let resp = file_deploy::UploadChunkResponse {
+                success: false,
+                offset: 0,
+                error: "Not authenticated".to_string(),
+            };
+            let packet = package_data(Command::CmdUploadChunk, resp);
+            self.post_send(packet).await;
+            return;
+        }
+        let upload_chunk_req = file_deploy::UploadChunkRequest::decode(payload);
+        if !upload_chunk_req.is_ok() {
+            let resp = file_deploy::UploadChunkResponse {
+                success: false,
+                offset: 0,
+                error: "Invalid request".to_string(),
+            };
+            let packet = package_data(Command::CmdUploadChunk, resp);
+            self.post_send(packet).await;
+            return;
+        }
+        let upload_chunk_req = upload_chunk_req.unwrap();
+        let write_result = {
+            let mut file_handles = self.file_handles.lock().await;
+            if !file_handles.contains_key(&upload_chunk_req.absolute_path) {
+                let resp = file_deploy::UploadChunkResponse {
+                    success: false,
+                    offset: upload_chunk_req.offset,
+                    error: "File not opened for upload".to_string(),
+                };
+                let packet = package_data(Command::CmdUploadChunk, resp);
+                self.post_send(packet).await;
+                return;
+            }
+            let file = file_handles
+                .get_mut(&upload_chunk_req.absolute_path)
+                .unwrap();
+            file.write_all(&upload_chunk_req.data).await
+        };
+        if write_result.is_err() {
+            let resp = file_deploy::UploadChunkResponse {
+                success: false,
+                offset: upload_chunk_req.offset,
+                error: format!("Failed to write data: {}", write_result.err().unwrap()),
+            };
+            let packet = package_data(Command::CmdUploadChunk, resp);
+            self.post_send(packet).await;
+            return;
+        }
+        if upload_chunk_req.is_last_chunk {
+            let mut file_handles = self.file_handles.lock().await;
+            file_handles.remove(&upload_chunk_req.absolute_path);
+        }
+        let resp = file_deploy::UploadChunkResponse {
+            success: true,
+            offset: upload_chunk_req.offset + upload_chunk_req.data.len() as u64,
+            error: "".to_string(),
+        };
+        let packet = package_data(Command::CmdUploadChunk, resp);
         self.post_send(packet).await;
     }
 
@@ -75,8 +256,17 @@ impl Session {
                 return Err("Checksum mismatch".into());
             }
             match command {
-                data_define::Command::CmdAuthenticate => {
+                Command::CmdAuthenticate => {
                     self.clone().on_auth(&payload_buf).await;
+                }
+                Command::CmdMkDir => {
+                    self.clone().on_mkdir(&payload_buf).await;
+                }
+                Command::CmdStartUpload => {
+                    self.clone().on_start_upload(&payload_buf).await;
+                }
+                Command::CmdUploadChunk => {
+                    self.clone().on_upload_chunk(&payload_buf).await;
                 }
             }
         }
@@ -87,30 +277,34 @@ impl Session {
         mut write_half: WriteHalf<TlsStream<TcpStream>>,
     ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         loop {
-            let mut local_queue = {
-                let mut queue = self.write_queue.lock().unwrap();
-                while queue.is_empty() {
-                    queue = self.cond_var.wait(queue).unwrap();
+            // 等待队列有数据
+            let data = loop {
+                let mut queue = self.write_queue.lock().await;
+                if let Some(data) = queue.pop_front() {
+                    // 队列有数据，取出一个包
+                    break data;
                 }
-                let mut q = VecDeque::new();
-                std::mem::swap(&mut *queue, &mut q);
-                q
+                drop(queue);
+                self.notify.notified().await;
             };
-            if let Some(data) = local_queue.pop_front() {
-                write_half.write_all(data.as_slice()).await?;
-                self.cond_var.notify_one();
-            }
+            // 发送数据
+            write_half.write_all(data.as_slice()).await?;
+            self.notify.notify_one();
         }
     }
 
     async fn post_send(&self, data: Vec<u8>) {
-        let mut queue = self.write_queue.lock().unwrap();
-        while queue.len() >= MAX_QUEUE_LEN {
-            // 队列满了，等待空间
-            queue = self.cond_var.wait(queue).unwrap();
+        loop {
+            let mut queue = self.write_queue.lock().await;
+            if queue.len() < MAX_QUEUE_LEN {
+                queue.push_back(data);
+                self.notify.notify_one();
+                break;
+            }
+            // 队列满了，释放锁，等待有空间
+            drop(queue);
+            self.notify.notified().await;
         }
-        queue.push_back(data);
-        self.cond_var.notify_one();
     }
 
     pub(crate) async fn run(
